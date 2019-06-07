f9f550be86ad7bd8007ef7c028ce1b8c5fde26a2
ccartagena879 <ccartagena879@gmail.com>
2019-06-06 22:32:51 -0500

    in class work

---
+++ StLouisSites2/Controllers/LocationController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StLouisSites2.Data;
+using StLouisSites2.Models;
+
+namespace StLouisSites2.Controllers
+{
+    public class LocationController : Controller
+    {
+        private ApplicationDbContext context;
+
+        public LocationController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Location> locations = context.Locations.ToList();
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Location location)
+        {
+            context.Add(location);
+            context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- StLouisSites2/Data/ApplicationDbContext.cs
+++ StLouisSites2/Data/ApplicationDbContext.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using StLouisSites2.Models;
 
 namespace StLouisSites2.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+
+        public DbSet<Location> Locations { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
