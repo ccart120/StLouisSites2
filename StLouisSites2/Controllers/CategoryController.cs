5dbbf6a01f1bdaa8f68ccdc600c1be6e3dcce08d
ccartagena879 <ccartagena879@gmail.com>
2019-06-28 22:23:22 -0500

    Category work

---
+++ StLouisSites2/Controllers/CategoryController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StLouisSites2.Data;
+using StLouisSites2.ViewModels.Category;
+
+namespace StLouisSites2.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ApplicationDbContext context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CategoryCreateViewModel categoryCreateViewModel)
+        {
+            int ID = CategoryCreateViewModel.CreateCategory(context, categoryCreateViewModel);
+            return View(categoryCreateViewModel);
+        }
+    }
+}
---
+++ StLouisSites2/ViewModels/Category/CategoryCreateViewModel.cs
@@ -0,0 +1,29 @@
+﻿using StLouisSites2.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StLouisSites2.ViewModels.Category
+{
+    public class CategoryCreateViewModel
+    {
+        private ApplicationDbContext context;
+
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public static int CreateCategory(ApplicationDbContext context, CategoryCreateViewModel categoryCreateViewModel )
+        {
+            Models.Category category = new Models.Category();
+            {
+                category.Name = categoryCreateViewModel.Name;
+            }
+            context.Add(category);
+            context.SaveChanges();
+
+            return category.ID;
+        }
+    }
+}
